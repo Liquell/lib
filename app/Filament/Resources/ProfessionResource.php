a4be700e10ea05617ffb06c485a5c2d3e1142184
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2024-03-17 23:06:49 +0200

    add: profession structure

---
+++ app/Filament/Resources/ProfessionResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProfessionResource\Pages;
+use App\Filament\Resources\ProfessionResource\RelationManagers;
+use App\Models\Profession;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProfessionResource extends Resource
+{
+    protected static ?string $model = Profession::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')->required(),
+                Forms\Components\Textarea::make('description'),
+                Forms\Components\FileUpload::make('image')->image(),
+            ])->columns(1);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\ImageColumn::make('image'),
+                Tables\Columns\TextColumn::make('title')->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProfessions::route('/'),
+            'create' => Pages\CreateProfession::route('/create'),
+            'edit' => Pages\EditProfession::route('/{record}/edit'),
+        ];
+    }
+}
