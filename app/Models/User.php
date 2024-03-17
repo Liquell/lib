7d9e4c45cff9f78ea71b2acdd44426ec78d47db4
Slavik Slinyuk <slinyuk.v@hexide-digital.com>
2024-03-17 22:57:14 +0200

    add: group structure

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,11 +12,6 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -24,23 +19,18 @@ class User extends Authenticatable
         'is_admin',
     ];
 
-    /**eeder
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function groups(): \Illuminate\Database\Eloquent\Relations\BelongsToMany
+    {
+        return $this->belongsToMany(Group::class);
+    }
 }
